872986566ba57fc5f6a01d7e3c4e57d146ade2a5
veenaprakash123 <veenaprakash2018@gmail.com>
2022-02-10 17:51:50 -0500

    Completed EntryDetail component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import Landing from './components/Landing'
+import {Link , Routes, Route} from 'react-router-dom'
 // import NewEntry from './components/NewEntry'
-// import EntryDetail from './components/EntryDetail'
+import EntryDetail from './components/EntryDetail'
 
 function App() {
   const [entries, setEntries] = useState([]);
-  const [entry, setEntry] = useState({});
+ 
 
-  //SHOW
+  // Landing
   let getEntries = async () => {
     let data = await fetch('http://localhost:7200/entries')
     let json = await data.json();
@@ -20,17 +21,26 @@ function App() {
     getEntries();
   }, []);
 
+  // Entry Details
+
+  // let getEntry = (entry) => {
+  //   setEntry(entry)
+  // }
+
   let addEntry = (entry) => {
     setEntries([...entries, entry]);
   };
 
-
   return (
     <div className="App">
         <h1>Allergy Busters</h1>
-        {/* <NewEntry setEntries={setEntries} addEntry={addEntry}/> */}
-        <Landing entries={entries} setEntries={setEntries}/>
-      {/* <EntryDetail entry={entry}/> */}
+        {/* <Landing entries={entries} setEntries={setEntries}/> */}
+      <main>
+          <Routes>
+            <Route path="/entries" element={<Landing entries={entries} setEntries={setEntries}/>}></Route>
+            <Route path="/entries/details/:id" element={<EntryDetail/>}></Route>
+          </Routes>
+      </main>
     </div>
   );
 }
