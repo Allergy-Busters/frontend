66f0a3ce134d393f4823a7c3faa64e0bcdd735bd
veenaprakash123 <veenaprakash2018@gmail.com>
2022-02-11 17:02:27 -0500

    Edit entry functionality is working

--- src/components/EditEntry.js
+++ src/components/EditEntry.js
@@ -5,85 +5,78 @@ import {  useParams } from 'react-router-dom'
 
 const EditEntry = () => {
 
-  const [entry, setEntry] = useState({});
+  const [entry, setEntry] = useState({
+    outdoorTemp: '',
+    visitOutside: '',
+    diet: '',
+    exercise: '',
+    potentialSymptoms: '',
+    img: '',
+    location: ''
+  });
 
   let {id} = useParams()
 
   useEffect(()=> {
     const editDetails = async() => {
-      let  entryResponse = await fetch('http://localhost:7200/entries/edit/' + id) 
+      let  entryResponse = await fetch('http://localhost:7200/entries/details/' + id) 
       let json = await entryResponse.json()
+      console.log(json)
       setEntry(json)
     }
     editDetails()
   }, [id]) 
 
-//   let handleChange = (e) => {
-//     setOutdoorTemp(e.target.value)
-//     setVisitOutside(e.target.value) 
-//     setDiet(e.target.value)
-//     setExercise(e.target.value)
-//     setPotentialSymptoms(e.target.value)
-//     setImg(e.target.value)
-//     setLocation(e.target.value)
-// }
 
-// let handleSubmit = async(e) => {
-//     e.preventDefault()
-//     // Adding new entry to our backend
-//     let response = await fetch('http://localhost:7200/entries/edit/' + id) {
-//         method: "PUT", 
-//         body: JSON.stringify(
-//             {
-//             outdoorTemp: outdoorTemp,
-//             visitOutside: true,
-//             diet: diet,
-//             exercise: exercise,
-//             potentialSymptoms: potentialSymptoms,
-//             img: img,
-//             location: location
-//         }
-//         // ^ Need to check correct syntax for this 
-//         ),
-//         headers: {
-//             'Content-Type':'application/json'
-//         }
-//         // ^ Do we need to include this?
-//     }
-// }
+  let handleChange = (e) => {
+   setEntry({...entry,[e.target.id]:e.target.value})
+}
+
+let handleSubmit = async(e) => {
+    e.preventDefault()
+    // Sending updated entry to our backend
+    let response = await fetch('http://localhost:7200/entries/edit/' + id, {
+        method: "PUT", 
+        body: JSON.stringify(entry),
+        headers: {
+            'Content-Type':'application/json'
+        }  
+    })
+    console.log(response)
+  }
 
   return (
-    <form>
+    <form onSubmit= {handleSubmit}>
       
             <label>Today's Date: </label>
-            <input type="text" id="date" name="date">{entry.date}</input>
+            <input type="text" id="date" name="date" value={entry.date} onChange={handleChange}></input>
 
             <label>Outdoor Temperature: </label>
-            <input type="text" id="outdoorTemp" name="outdoorTemp" >{entry.outdoorTemp}</input>
+            <input type="text" id="outdoorTemp" name="outdoorTemp" value={entry.outdoorTemp} onChange={handleChange}></input>
         
             <label>Visit outside: </label>
             <input type="checkbox" id="visitOutside" name="visitOutside"/>
        
             <label htmlFor="diet">Diet: </label>
-            <input type="text" id="diet" name="diet" >{entry.diet}</input>
+            <input type="text" id="diet" name="diet" value={entry.diet} onChange={handleChange}></input>
         
             <label htmlFor="exercise">Exercise: </label>
-            <input type="text" id="exercise" name="exercise" >{entry.exercise}</input>
+            <input type="text" id="exercise" name="exercise" value={entry.exercise} onChange={handleChange} ></input>
         
             <label htmlFor="potentialSymptoms">How are you feeling?: </label>
-            <input type="text" id="potentialSymptoms" name="potentialSymptoms" >{entry.potentialSymptoms}</input>
+            <input type="text" id="potentialSymptoms" name="potentialSymptoms" value={entry.potentialSymptoms} onChange={handleChange}></input>
         
             <label htmlFor="img">Upload Picture: </label>
-            <input type="text" id="img" name="img">{entry.img}</input>
+            <input type="text" id="img" name="img" value={entry.img} onChange={handleChange}></input>
         
             <label htmlFor="location">Location: </label>
-            <input type="text" id="location" name="location">{entry.location}</input>
+            <input type="text" id="location" name="location" value={entry.location} onChange={handleChange}></input>
         
             <input type="submit" value="Enter"/>
       
-
   </form>
   )
 }
 
-export default EditEntry
+
+export default EditEntry;
