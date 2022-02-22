d5e6d4ba0883335c86f557f924b79b366855667d
veenaprakash123 <veenaprakash2018@gmail.com>
2022-02-22 02:44:13 -0500

    About page updated and READme file included. screenshots have not yet been added to READme

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import '../CSS/About.css'
-import '../peopleImages/matt.png'
-import '../peopleImages/veena.png'
-import '../peopleImages/quentin.png'
+import Matt from '../images/matt.png'
+import Veena from '../images/veena.png'
+import Quentin from '../images/quentin.png'
 import { Card } from 'react-bootstrap'
 
 const About = () => {
@@ -10,20 +10,24 @@ const About = () => {
     <div>
         <h2 className="header">About</h2>
         
-        <Card className="about" style={{width: '40rem' , height: '15rem'}}> 
+        <Card className="about" style={{width: '40rem' , height: '19rem'}}> 
             <Card.Text>
-            Welcome to Allergy Busters! 
+            <p>Welcome to Allergy Busters!</p>
             
-            Allergy Busters allows you to track your daily habits, environmental factors, and potential irritants in order to help you and your doctor better understand the causes of a particular allergy/intolerance. This can ultimately result in a proper treatment or avoidance plan. 
+            <p>Our app allows you to track your daily habits, environmental factors, and potential irritants in order to help you and your doctor better understand the causes of a particular allergy/intolerance. This can ultimately result in a proper treatment or avoidance plan.</p>
             
-            Simply add an entry at the end of each day, jotting down the food you consumed, any exercise you took part in and how you feel. This routine, after establishing consistency, will help you to catch patterns - do you generally feel sick on days you consume bread? Do you break out into hives everytime you go for a run in 75 degree weather? Allergy Busters will equip you to get to the bottom of it! 
+            <p> Simply add an entry at the end of each day. Jot down the food you consumed, any exercise you took part in and how you feel. This routine, after establishing consistency, will help you identify patterns - do you generally feel sick on days you consume bread? Do you break out into hives everytime you go for a run in 75 degree weather? Allergy Busters will equip you to get to the bottom of it!</p>
             </Card.Text>
         </Card>
 
+        <div>
+            <h3 className="header-team" style={{textDecorationLine: 'underline'}}>The Team</h3>
+        </div>
+
         <div className="ourPhotos">
-            <image src='../peopleImages/matt.jpg' alt="Matt" height="300px" width="300px" /> 
-            <image src='../peopleImages/veena.jpg' alt="Veena"/> 
-            <image src='../peopleImages/quentin.jpg' alt="Quentin"/> 
+            <img src={Matt} alt="Matt" height={250} width={250} /> 
+            <img src={Veena} alt="Veena" height={250} width={250}/> 
+            <img src={Quentin} alt="Quentin" height={250} width={250}/> 
         </div>
 
     </div>
