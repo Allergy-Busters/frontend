ceaf13a3dd78a8632d149d24a6f39750b4d729e6
mgholden <mgholden27@gmail.com>
2022-02-24 23:08:45 -0500

    Cleaning up 2.24.22

--- src/components/NewEntry.js
+++ src/components/NewEntry.js
@@ -35,8 +35,7 @@ const NewEntry = ({addEntry}) => {
 
     let handleSubmit = async(e) => {
         e.preventDefault()
-        console.log(entry)
-        // console.log("What we are looking for")
+        //console.log(entry)
         // Adding new entry to our backend
         const url = process.env.REACT_APP_ENV === 'production' ? 'https://allergybusters-backend.herokuapp.com' : 'http://localhost:7200/entries'
         let response = await fetch(url, {
@@ -52,7 +51,6 @@ const NewEntry = ({addEntry}) => {
         console.log(newEntry)
         addEntry(newEntry)
         navigate('/entries')
-
     }
 
     let handleChange = (e) => {
@@ -68,99 +66,63 @@ const NewEntry = ({addEntry}) => {
         setEntry({...entry, visitOutside:!entry.visitOutside})
     }
 
-
-
-
   return (
     <div className='body body-newEntry' style={{alignContent:'left'}}>
-    <Card className="mb-3" style={{width: '21rem' , height: '41rem'}}>
-    <Card.Body className="mb-2" style={{width: '20rem'}}> 
-     <Form onSubmit={handleSubmit} encType="multipart/form-data">
-        {/* <form onSubmit={handleSubmit} encType="multipart/form-data"> */}
-        
-            <Form.Group className="mb-3" controlId="formGroupEmail">
+      <Card className="mb-3" style={{width: '21rem' , height: '41rem'}}>
+        <Card.Body className="mb-2" style={{width: '20rem'}}> 
+          <Form onSubmit={handleSubmit} encType="multipart/form-data">
+            <Form.Group className="mb-3" controlId="formTodaysDate">
             <Form.Label  className="label" htmlFor="todaysDate"> Today's Date:</Form.Label>
             <Form.Control type="text" id="date" name="date" placeholder="Ex: 01/01/2022" onChange={handleChange}></Form.Control>
             </Form.Group> 
 
-            <Form.Group className="mb-3" controlId="formGroupEmail">
+            <Form.Group className="mb-3" controlId="formOutdoorTemp">
             <Form.Label className="label" htmlFor="outdoorTemp">Outdoor Temperature:</Form.Label>
             <Form.Control type="text" id="outdoorTemp" name="outdoorTemp" placeholder="In ℉ or ℃" onChange={handleChange}></Form.Control>
             </Form.Group>  
 
             <Form.Group className="mb-3" id="formGridCheckbox">
             <Form.Check className="label" label="Visit Outside" type="checkbox"></Form.Check>
-            {/* <Form.Control type="checkbox" id="visitOutside" name="visitOutside" onChange={toggleCheckbox} checked={!entry.visitOutside}></Form.Control> */}
             </Form.Group>  
 
-            <Form.Group className="mb-3" controlId="formGroupEmail">
+            <Form.Group className="mb-3" controlId="formDiet">
             <Form.Label className="label" htmlFor="diet">Diet: </Form.Label>
             <Form.Control type="text" id="diet" name="diet" placeholder="Ex: eggs, milk, bread" onChange={handleChange}></Form.Control>
             </Form.Group>  
 
-            <Form.Group className="mb-3" controlId="formGroupEmail">
+            <Form.Group className="mb-3" controlId="formExercise">
             <Form.Label className="label" htmlFor="exercise">Exercise: </Form.Label>
             <Form.Control type="text" id="exercise" name="exercise" placeholder="Ex: Went for a run" onChange={handleChange}></Form.Control>
             </Form.Group> 
 
-            <Form.Group className="mb-3" controlId="formGroupEmail">
+            <Form.Group className="mb-3" controlId="formSymptoms">
             <Form.Label className="label" htmlFor="potentialSymptoms">How are you feeling?: </Form.Label>
             <Form.Control type="text" id="potentialSymptoms" name="potentialSymptoms" placeholder="Ex: My stomach aches" onChange={handleChange}></Form.Control>
             </Form.Group>  
 
-            <Form.Group className="mb-3" controlId="formGroupEmail">
+            <Form.Group className="mb-3" controlId="formImage">
             <Form.Label className="label" htmlFor="img">Upload Picture: </Form.Label>
             <Form.Control type="file" id="img" name="img" placeholder="Ex:image of rash" onChange={fileChangeHandler}></Form.Control>
             </Form.Group>  
             
-            <Form.Group className="mb-3" controlId="formGroupEmail">
+            <Form.Group className="mb-3" controlId="formLocation">
             <Form.Label className="label" htmlFor="location">Location: </Form.Label>
             <Form.Control type="text" id="location" name="location" placeholder="Ex:local farm" onChange={handleChange}></Form.Control>
             </Form.Group>  
-
-
-            
-            {/* <label htmlFor="todaysDate">Today's Date: </label>
-            <input type="text" id="date" name="date" placeholder="01/01/2022" onChange={handleChange}/>
-
-            <label htmlFor="outdoorTemp">Outdoor Temperature: </label>
-            <input type="text" id="outdoorTemp" name="outdoorTemp" placeholder="In ℉ or ℃" onChange={handleChange}/>
-        
-            <label htmlFor="visitOutside">Visit Outside: </label>
-            <input type="checkbox" id="visitOutside" name="visitOutside" onChange={toggleCheckbox} checked={!entry.visitOutside} />
-       
-            <label htmlFor="diet">Diet: </label>
-            <input type="text" id="diet" name="diet" placeholder="Ex: eggs, milk, bread" onChange={handleChange}></input>
-        
-            <label htmlFor="exercise">Exercise: </label>
-            <input type="text" id="exercise" name="exercise" placeholder="Ex: Went for a run" onChange={handleChange}></input>
-        
-            <label htmlFor="potentialSymptoms">How are you feeling?: </label>
-            <input type="text" id="potentialSymptoms" name="potentialSymptoms" placeholder="Ex: My stomach aches" onChange={handleChange}></input>
-        
-            <label htmlFor="img">Upload Picture: </label>
-            <input type="file" id="img" name="img" placeholder="Ex:image of rash" onChange={fileChangeHandler}></input>
-        
-            <label htmlFor="location">Location: </label>
-            <input type="text" id="location" name="location" placeholder="Ex:local farm" onChange={handleChange}></input> */}
-
     
             <div className="entryNew-buttons">
-            <Button size="lg" className="editEntry-button" type ="submit" variant="dark">
-              Add New Entry
-            </Button>
-            <Button size="lg" variant="dark">
-            <Link to={'/entries'} style={{textDecoration: 'none', color: 'white'}}>Cancel</Link>
-            </Button>
+              <Button size="lg" className="editEntry-button" type ="submit" variant="dark">
+                Add New Entry
+              </Button>
+              <Button size="lg" variant="dark">
+                <Link to={'/entries'} style={{textDecoration: 'none', color: 'white'}}>Cancel</Link>
+              </Button>
             </div>
-              {/* </form> */}
-        </Form>
+          </Form>
         </Card.Body>
       </Card>
     </div>
           
-
-
   )
 };
 
